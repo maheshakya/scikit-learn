720152b9bd491f101655ede8062af203697a319b
maheshakya <pmaheshakya4@gmail.com>
2014-03-11 22:40:33 +0530

    Update __init__.py

--- sklearn/ensemble/__init__.py
+++ sklearn/ensemble/__init__.py
@@ -9,12 +9,14 @@ from .forest import RandomForestRegressor
 from .forest import RandomTreesEmbedding
 from .forest import ExtraTreesClassifier
 from .forest import ExtraTreesRegressor
-from .bagging import BaggingClassifier
-from .bagging import BaggingRegressor
 from .weight_boosting import AdaBoostClassifier
 from .weight_boosting import AdaBoostRegressor
 from .gradient_boosting import GradientBoostingClassifier
 from .gradient_boosting import GradientBoostingRegressor
+from .bagging import BaggingClassifier
+from .bagging import BaggingRegressor
+from .bagging import _partition_estimators
+from .bagging import _parallel_build_estimators
 
 from . import bagging
 from . import forest
@@ -22,11 +24,25 @@ from . import weight_boosting
 from . import gradient_boosting
 from . import partial_dependence
 
-__all__ = ["BaseEnsemble",
-           "RandomForestClassifier", "RandomForestRegressor",
+__all__ = ["BaseEnsemble", "RandomForestClassifier", "RandomForestRegressor",
            "RandomTreesEmbedding", "ExtraTreesClassifier",
-           "ExtraTreesRegressor", "BaggingClassifier",
-           "BaggingRegressor", "GradientBoostingClassifier",
+           "ExtraTreesRegressor", "GradientBoostingClassifier",
            "GradientBoostingRegressor", "AdaBoostClassifier",
-           "AdaBoostRegressor", "bagging", "forest", "gradient_boosting",
-           "partial_dependence", "weight_boosting"]
+           "AdaBoostRegressor",  "bagging", "forest", "gradient_boosting",
+           "partial_dependence", "weight_boosting", "BaggingClassifier", "BaggingRegressor"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
